e2b26b37bb669e155563ac57cb01397797eeeddb
Richard Ottaway <richard.ottaway5@gmail.com>
2020-07-31 11:04:15 +0100

    Changed order

--- DangerTapeDB/Tables/ModeNotes.sql
+++ DangerTapeDB/Tables/ModeNotes.sql
@@ -2,7 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [ModeId] INT NOT NULL, 
-    [NoteNumber] INT NOT NULL, 
     [NoteOrder] INT NOT NULL, 
+    [NoteNumber] INT NOT NULL, 
     CONSTRAINT [FK_ModeNotes_ToModes] FOREIGN KEY (ModeId) REFERENCES [Modes]([Id])
 )
